d83df1ed88f289f08e8fd91d06ce1419517f4c48
Lucas-X-A <lucasxavierdealmeida@gmail.com>
2025-06-26 23:16:02 -0300

    feat: adição de mais tags do imóvel

--- src/main/java/com/mewebstudio/javaspringbootboilerplate/entity/Caracteristica.java
+++ src/main/java/com/mewebstudio/javaspringbootboilerplate/entity/Caracteristica.java
@@ -4,10 +4,25 @@ import lombok.Getter;
 
 @Getter
 public enum Caracteristica {
+
     MOBILIADO("Mobiliado"),
     AR_CONDICIONADO("Ar Condicionado"),
-    INTERNET("Internet");
-
+    INTERNET("Internet"),
+    ACEITA_PETS("Aceita Pets"),
+    PISCINA("Piscina"),
+    CHURRASQUEIRA("Churrasqueira"),
+    ACADEMIA("Academia"),
+    GARAGEM("Garagem"),
+    VARANDA("Varanda"),
+    AREA_DE_SERVICO("Área de serviço"),
+    PORTARIA_24H("Portaria 24h"),
+    SALAO_DE_FESTAS("Salão de festas"),
+    ELEVADOR("Elevador"),
+    GAS_ENCANADO("Gás encanado"),
+    ARMARIOS_EMBUTIDOS("Armários embutidos"),
+    LAVANDERIA_NO_LOCAL("Lavanderia no Local"),
+    BICICLETARIO("Bicicletário"),
+    TRANSPORTE_PUBLICO_PROXIMO("Transporte Público Próximo");
 
     private final String descricao;
 
