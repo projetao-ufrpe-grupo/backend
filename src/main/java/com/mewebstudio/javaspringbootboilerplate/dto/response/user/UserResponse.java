6422a7f9465fda57c633bb1ceda76ce78d1883e5
Lucas-X-A <lucasxavierdealmeida@gmail.com>
2025-07-04 19:18:00 -0300

    fix: atualização do retorno do usuário

--- src/main/java/com/mewebstudio/javaspringbootboilerplate/dto/response/user/UserResponse.java
+++ src/main/java/com/mewebstudio/javaspringbootboilerplate/dto/response/user/UserResponse.java
@@ -9,6 +9,7 @@ import lombok.experimental.SuperBuilder;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Getter
 @Setter
@@ -55,20 +56,59 @@ public class UserResponse extends AbstractBaseResponse {
     private String tipoUsuario;
 
     @Schema(
+        name = "fotoPerfil",
+        description = "Profile photo in Base64 format", 
+        type = "String"
+        )
+    private String fotoPerfil;
+
+    @Schema(
+        name = "biografia",
+        description = "User's biography", 
+        type = "String", 
+        example = "Estudante de Ciência da Computação na UFMG."
+        )
+    private String biografia;
+
+    @Schema(
+        name = "semestre",
+        description = "Current semester of the student", 
+        type = "Integer", 
+        example = "5"
+        )
+    private Integer semestre;
+
+    @Schema(
+        name = "curso",
+        description = "User's course", 
+        type = "String", 
+        example = "Ciência da Computação"
+        )
+    private String curso;
+
+    @Schema(
+        name = "regiaoDeInteresse",
+        description = "User's region of interest", 
+        type = "String", 
+        example = "Pampulha, Belo Horizonte - MG"
+        )
+    private String regiaoDeInteresse;
+
+    @Schema(
+        name = "interesses",
+        description = "List of user's interests", 
+        type = "List<String>", 
+        example = "[\"JOGOS\", \"TECNOLOGIA\"]"
+        )
+    private List<String> interesses;
+
+    @Schema(
         name = "emailVerifiedAt",
         description = "Date time field of user e-mail verified",
         type = "LocalDateTime",
         example = "2022-09-29T22:37:31"
     )
     private LocalDateTime emailVerifiedAt;
-
-    @Schema(
-        name = "blockedAt",
-        description = "Date time field of user blocked",
-        type = "LocalDateTime",
-        example = "2022-09-29T22:37:31"
-    )
-    private LocalDateTime blockedAt;
 
     @Schema(
         name = "createdAt",
@@ -98,8 +138,14 @@ public class UserResponse extends AbstractBaseResponse {
             .name(user.getName())
             .roles(user.getRoles().stream().map(role -> role.getName().name()).toList())
             .tipoUsuario(user.getTipoUsuario() != null ? user.getTipoUsuario().name() : null)
+            .fotoPerfil(user.getFotoPerfil())
+            .biografia(user.getBiografia())
+            .semestre(user.getSemestre())
+            .curso(user.getCurso())
+            .regiaoDeInteresse(user.getRegiaoDeInteresse())
+            .interesses(user.getInteresses() != null ?
+                user.getInteresses().stream().map(Enum::name).collect(Collectors.toList()) : null)
             .emailVerifiedAt(user.getEmailVerifiedAt())
-            .blockedAt(user.getBlockedAt())
             .createdAt(user.getCreatedAt())
             .updatedAt(user.getUpdatedAt())
             .build();
