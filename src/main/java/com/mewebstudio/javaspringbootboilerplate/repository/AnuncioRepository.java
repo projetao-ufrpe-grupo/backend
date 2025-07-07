9e0a45e497942de9dca4448e3bcaad2462d6f91d
Lucas-X-A <140546986+Lucas-X-A@users.noreply.github.com>
2025-07-07 14:24:00 -0300

    Update AnuncioRepository.java

--- src/main/java/com/mewebstudio/javaspringbootboilerplate/repository/AnuncioRepository.java
+++ src/main/java/com/mewebstudio/javaspringbootboilerplate/repository/AnuncioRepository.java
@@ -1,20 +1,29 @@
 package com.mewebstudio.javaspringbootboilerplate.repository;
 
-import com.mewebstudio.javaspringbootboilerplate.entity.Anuncio;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-import java.util.UUID;
+import com.mewebstudio.javaspringbootboilerplate.entity.Anuncio;
 
 @Repository
 public interface AnuncioRepository extends JpaRepository<Anuncio, UUID> {
-    // Query 1: Busca os anúncios e as coleções que não são List
-    @Query("SELECT DISTINCT a FROM Anuncio a LEFT JOIN FETCH a.imovel i LEFT JOIN FETCH a.anunciante")
-    List<Anuncio> findAllWithImovelAndAnunciante();
+    
+    @Query("SELECT DISTINCT a FROM Anuncio a " +
+           "LEFT JOIN FETCH a.imovel i " +
+           "LEFT JOIN FETCH i.caracteristicas " +
+           "LEFT JOIN FETCH i.fotos")
+    List<Anuncio> findAllWithImovelAndCaracteristicas();
 
-    // Query 2: Busca os anúncios novamente, mas agora com as coleções List
-    @Query("SELECT DISTINCT a FROM Anuncio a LEFT JOIN FETCH a.imovel i LEFT JOIN FETCH i.fotos LEFT JOIN FETCH i.caracteristicas WHERE a IN :anuncios")
-    List<Anuncio> findAllWithCollections(List<Anuncio> anuncios);
+    @Query("SELECT DISTINCT a FROM Anuncio a " +
+           "LEFT JOIN FETCH a.imovel i " +
+           "LEFT JOIN FETCH i.caracteristicas " +
+           "LEFT JOIN FETCH i.fotos " +
+           "WHERE a.id = :id")
+    Optional<Anuncio> findByIdWithImovelAndCaracteristicas(@Param("id") UUID id);
 }
